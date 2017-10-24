401dd9aa54cad96e71148d4fdbcb7f685304f047
hippykid88 <33055337+hippykid88@users.noreply.github.com>
2017-10-24 11:47:33 -0700

    Add files via upload

--- cron.py
+++ cron.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 #toos to build your cron job
 
 import getpass
@@ -8,11 +7,13 @@ import os
 
 
 def cron_time():
+    minute = input( 'what minute on the hour do you want this to run?(pick 1-60): ' )
+    hour = input( 'what hour would you like this to run(pick from 1-24)?: ' )
     os.getcwd() #grabs current working directory path
     username = getpass.getuser() #grabs username to run cron under
     my_cron = CronTab( user=username)
     job = my_cron.new( command='python ' + os.getcwd() + "/" + '%s')
-    job.minute.on('%s')
-    job.hour.on('%s')
+    job.minute.on('%s') % (minute)
+    job.hour.on('%s') % (hour)
 
     my_cron.write()
